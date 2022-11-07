12445f57e0a29cfeceb7444b7a7d2d46153d8754
Alex Solovey <naghtigall@gmail.com>
2022-11-07 16:25:18 +0300

    Root download directory condition
    Added condition of checking empty query or root download directory

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-    "log"
+	"html/template"
+	"log"
 	"net/http"
-    "html/template"
 )
 
-
 const (
-    domain = "go-study.site"
-    blogDomain = "blog." + domain
-    adminDomain = "admin." + domain
-    designDomain = "design." + domain
+	domain       = "localhost" //Don't forget to change URL
+	blogDomain   = "blog." + domain
+	adminDomain  = "admin." + domain
+	designDomain = "design." + domain
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        http.NotFound(w, r)
-        return
-    }
-    // Initialize a slice containing the paths to the two files. Note that the
-    // home.page.tmpl file must be the *first* file in the slice.
-    files := []string{
-        "./ui/html/menu.page.tmpl",
-        "./ui/html/home.page.tmpl",
-        "./ui/html/base.layout.tmpl",
-    }
-    // Use the template.ParseFiles() function to read the files and store the
-    // templates in a template set. Notice that we can pass the slice of file paths
-    // as a variadic parameter?
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-        return
-    }
-    err = ts.Execute(w, nil)
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-    }
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	// Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
+	files := []string{
+		"./ui/html/menu.page.tmpl",
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+	}
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we can pass the slice of file paths
+	// as a variadic parameter?
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func designIndex(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Designa maina domain"))
+	w.Write([]byte("Designa maina domain"))
 }
 
 func blogIndex(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Blog domain"))
+	w.Write([]byte("Blog domain"))
 }
 func adminIndex(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Admin domain"))
+	w.Write([]byte("Admin domain"))
 }
 
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
-    var file string
-    file = "NaN"
-    file = r.URL.Query().Get("file")
-    log.Println(file)
-    if file == "NaN" {
-        http.NotFound(w, r)
-        return
-    }else{
-        http.ServeFile(w, r, "./ui/downloads/"+file)
-    }
+	var file string
+	file = "NaN"
+	file = r.URL.Query().Get("file")
+	log.Println(file)
+	if file == "NaN" || file == "" {
+		//Added condition of checking empty query or root download directory - || file == ""
+		http.NotFound(w, r)
+		return
+	} else {
+		http.ServeFile(w, r, "./ui/downloads/"+file)
+	}
 }
